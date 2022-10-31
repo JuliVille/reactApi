8a4b53343e143eb0a2c457ccf27c6052dbe6fd13
Julian <julianvillero16@gmail.com>
2022-10-30 20:45:17 -0500

    paginacion completada

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -2,21 +2,33 @@ import React from "react";
 
 const PeticionApi = () => {
     const [personajes, setPersonajes] = React.useState([]);
-    //const [paginacion, setPaginacion] = React.useState(1);
+    const [paginacion, setPaginacion] = React.useState(1);
     
     const getPersonajes = async() =>{
         try{
-            const res = await fetch("https://randomuser.me/api/");
+            const res = await fetch(`https://randomuser.me/api/?page=${paginacion}`);
             const {results} = await res.json();
             setPersonajes(results);
         }catch(error){
             console.log(error);
         }
     }
+
+    const siguiente = async () =>{
+        await setPaginacion(paginacion + 1);
+        getPersonajes();
+    }
+
+    const atras = async () =>{
+        await setPaginacion(paginacion - 1)
+        getPersonajes();
+    }
     return (
         <div>
             <h1>PETICION API</h1>
             <button onClick={getPersonajes}>Traer Personajes</button>
+            <button onClick={siguiente}>Siguiente</button>
+            <button onClick={atras}>Atras</button>
             {
                 personajes.map((resultado) =>(
                     <div>
